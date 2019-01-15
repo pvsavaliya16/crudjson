a1f1aad183ac24d28d8e6eb82422dd985ffdb15b
pvsavaliya16 <pvsavaliya16@gmail.com>
2019-01-15 18:33:27 +0530

    submitted

--- src/app/student.ts
+++ src/app/student.ts
@@ -5,6 +5,6 @@ export class Student {
     branch: string;
 }
 
-export const Students1 =[{}]
+export const Students1 =[{ "eid": "150470107038", "name": "Parth", "sem": "8", "branch": "CE" }];
 
 export const Students: Student[] = [];
